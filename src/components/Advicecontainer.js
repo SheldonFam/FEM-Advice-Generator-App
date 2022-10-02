3263655b85bcf6c92ea2f437663ff1d07be4505d
Sheldon Fam <sheldonfam95@gmail.com>
2022-10-02 16:41:25 +0800

    revise styling for main tag

--- src/components/Advicecontainer.js
+++ src/components/Advicecontainer.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import dice from "../images/icon-dice.svg";
 import destopdivider from "../images/pattern-divider-desktop.svg";
 import mobiledivider from "../images/pattern-divider-mobile.svg";
+import Button from "./Button";
 
-const Advicecontainer = (props) => {
+const Advicecontainer = () => {
   const URL = "https://api.adviceslip.com/advice";
   const [quote, setQuote] = useState({
     id: 117,
@@ -23,28 +23,21 @@ const Advicecontainer = (props) => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-darkBlue font-manrope font-bold text-lightCyan">
+    <div className="flex flex-col items-center justify-center h-screen bg-darkBlue font-manrope font-bold text-lightCyan ">
       <main className="flex flex-col items-center justify-center p-5 bg-darkGrayishBlue rounded-[10px] w-[343px] h-[316px] relative md:w-[540px]">
-        <div className="text-center items-center">
-          <h1 className="text-neonGreen text-headingS md:text-heading uppercase mb-6">
-            Advice # {quote.id}
-          </h1>
-          <p className="text-quoteS md:text-quote md:h-[150px] pb-5 px-5 mb-5">
-            “{quote.advice}”
-          </p>
-          <div className="md:hidden mb-5">
-            <img className="inline" src={mobiledivider} alt="divider" />
-          </div>
-          <div className="hidden md:block mb-10">
-            <img className="inline" src={destopdivider} alt="destopdivider" />
-          </div>
+        <h1 className="text-neonGreen text-headingS md:text-heading uppercase mb-6">
+          Advice # {quote.id}
+        </h1>
+        <p className="text-quoteS md:text-quote md:h-[150px] px-5 mb-5 text-center">
+          “{quote.advice}”
+        </p>
+        <div className="md:hidden mb-5">
+          <img className="inline" src={mobiledivider} alt="divider" />
         </div>
-        <button
-          className="transition hover:shadow-shadow hover:shadow-neonGreen cursor-pointer absolute bottom-[-32px] rounded-full bg-neonGreen w-[64px] h-[64px] flex justify-center items-center "
-          onClick={fetchAdvice}
-        >
-          <img src={dice} alt="dice" />
-        </button>
+        <div className="hidden md:block mb-5">
+          <img className="inline" src={destopdivider} alt="destopdivider" />
+        </div>
+        <Button onClick={fetchAdvice} />
       </main>
     </div>
   );
